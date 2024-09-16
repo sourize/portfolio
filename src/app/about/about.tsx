d1c486afeb800a704318a6cc2e23d7cc62d448ee
sourize <sourishchatterjee0067@gmail.com>
2024-09-17 01:02:57 +0530

    File Changes

--- src/app/about/about.tsx
+++ src/app/about/about.tsx
@@ -10,6 +10,6 @@ export const INFO = {
   Toolkit: "AI/ML Wizardry: Bringing ideas to life using neural networks, random forests, and other ML models. Python is my go-to language.\nData Whisperer: Transforming raw data into actionable insights with Pandas, Numpy, and Matplotlib.\nTeam Player: Collaborating effectively through internships, hackathons, and group projects.",
 
   connect:
-    "Let's connect and build something cool together. Feel free to reach out to me. I'm always open to new opportunities and collaborations. Reach out to me via **email: (contact.sourishchatterjeer@gmail.com)** or dm me on [Twitter](https://twitter.com/sourize_) or [Instagram](https://www.instagram.com/sourish.ml/).",
+    "Let's connect and build something cool together. Feel free to reach out to me. I'm always open to new opportunities and collaborations. Reach out to me via **email: (contact.sourishchatterjeer@gmail.com)** or dm me on [Twitter](https://twitter.com/sourize_) or [Instagram](https://www.instagram.com/sourish.ml/).\n\n\n",
 }
 
