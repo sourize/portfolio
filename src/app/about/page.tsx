e5740f35bc52c2fa59647c198ad6c97d8bcbf0af
sourize <sourishchatterjee0067@gmail.com>
2024-09-17 12:54:36 +0530

    File Changes

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -20,7 +20,7 @@ export default function About() {
           delay={BLUR_FADE_DELAY}
           className="text-3xl font-bold tracking-tighter sm:text-5xl xl:text-6xl/none"
           yOffset={8}
-          text={`Hey There, I'm ${DATA.name} 👋🏼`}
+          text={`Hey There, I'm Sourish 👋🏼`}
         />
         <BlurFade delay={BLUR_FADE_DELAY * 2}>
           <p className="text-lg text-muted-foreground mb-6">
