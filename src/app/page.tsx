ce3bf6bea24fffacab5c562d374ecb35664eb2ab
sourize <sourishchatterjee0067@gmail.com>
2024-09-20 22:06:11 +0530

    File Changes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -61,12 +61,12 @@ export default function Page() {
             <div className="flex-col flex flex-1 space-y-1.5">
               <BlurFadeText
                 delay={BLUR_FADE_DELAY}
-                className="text-2xl font-bold tracking-tighter sm:text-4xl xl:text-5xl/none"
+                className="text-3xl font-bold tracking-tighter sm:text-4xl xl:text-5xl/none"
                 yOffset={8}
-                text={`**Hi, I'm ${DATA.name.split(" ")[0]} 👋**`}
+                text={`Hi, I'm ${DATA.name.split(" ")[0]} 👋`}
               />
               <BlurFadeText
-                className="max-w-[600px] text-sm md:text-base mt-2"
+                className="max-w-[600px] md:text-lg mt-2"
                 delay={BLUR_FADE_DELAY}
                 text={DATA.description}
               />
@@ -288,10 +288,6 @@ export default function Page() {
             Template by{" "}
             <Link href="https://x.com/dillionverma" className="text-blue-500 hover:underline">
               Dhillon Verma
-            </Link>
-            , modified by{" "}
-            <Link href="https://x.com/SaidevDhal" className="text-blue-500 hover:underline">
-              Saidev Dhal
             </Link>
           </p>
         </BlurFade>
