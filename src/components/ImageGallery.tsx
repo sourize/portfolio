f5b1e137880cff6b04cde8daba947fd2f54e6bac
sourize <sourishchatterjee0067@gmail.com>
2024-09-20 20:55:01 +0530

    File Changes

--- src/components/ImageGallery.tsx
+++ src/components/ImageGallery.tsx
@@ -12,9 +12,9 @@ const images = [
 export function ImageGallery() {
   return (
     <BlurFade delay={0.1}>
-      <div className="grid grid-cols-2 md:grid-cols-2 gap-1">
+      <div className="grid grid-cols-2 md:grid-cols-3 gap-2">
         {images.map((img, index) => (
-          <div key={index} className={`relative ${index === 3 ? 'col-span-2 row-span-2' : ''}`}>
+          <div key={index} className={`relative ${index === 2 ? 'col-span-2 row-span-2' : ''}`}>
             <Image
               src={img.src}
               alt={img.alt}
