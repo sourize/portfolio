45fb51d3caf509827439e91f06dccd7cb7948158
sourize <sourishchatterjee0067@gmail.com>
2024-10-11 15:15:17 +0530

    File Changes

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,6 +6,7 @@ import { cn } from "@/lib/utils";
 import type { Metadata } from "next";
 import { Inter as FontSans } from "next/font/google";
 import "./globals.css";
+import Particles from './Particles'; // Ensure this path is correct
 
 const fontSans = FontSans({
   subsets: ["latin"],
@@ -63,6 +64,7 @@ export default function RootLayout({
       >
         <ThemeProvider attribute="class" defaultTheme="light">
           <TooltipProvider delayDuration={0}>
+            <Particles /> {/* Adding Particles component */}
             {children}
             <Navbar />
           </TooltipProvider>
