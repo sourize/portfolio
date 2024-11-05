154f64bf0c1fb0b22663b532f9c3900d164958d2
sourize <sourishchatterjee0067@gmail.com>
2024-11-05 20:07:38 +0530

    File Updated

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -170,7 +170,7 @@ export default function Page() {
             <div className="space-y-12 w-full">
               <div className="flex flex-col items-center justify-center space-y-4 text-center">
                 <div className="space-y-2">
-                  <h2 className="text-3xl font-bold tracking-tighter sm:text-5xl mt-2">
+                  <h2 className="text-3xl font-bold tracking-tighter sm:text-4xl mt-2">
                     Some of my projects
                   </h2>
                   <p className="text-muted-foreground md:text-xl/relaxed lg:text-base/relaxed xl:text-xl/relaxed">
