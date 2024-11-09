13ce6d27306e84d6a4abae1c83d128aee0e8d44b
sourize <sourishchatterjee0067@gmail.com>
2024-11-10 01:06:42 +0530

    Files Updated

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -56,7 +56,7 @@ export default function Page() {
 
   return (
     <main className="flex flex-col min-h-[100dvh] space-y-10">
-      <section id="hero">
+      <section id="hero" className="mt-10">
         <div className="mx-auto w-full max-w-2xl space-y-8">
           <div className="gap-2 flex justify-between">
             <div className="flex-col flex flex-1 space-y-1.5">
