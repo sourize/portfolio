e0b5613f576517a84d993c26e2c454ae4e3063ff
sourize <sourishchatterjee0067@gmail.com>
2024-11-10 01:23:31 +0530

    Files Updated

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -86,7 +86,7 @@ export default function Page() {
           <h2 className="text-2xl font-bold">About Me</h2>
         </BlurFade>
         <BlurFade delay={BLUR_FADE_DELAY * 4}>
-          <Markdown className="prose max-w-full text-pretty font-sans text-base text-muted-foreground dark:prose-invert justify tracking tighter">
+          <Markdown className="prose max-w-full text-pretty font-sans text-sm text-muted-foreground dark:prose-invert">
             {DATA.summary}
           </Markdown>
         </BlurFade>
