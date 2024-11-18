02d4ba37a95e093194d9fc6be85525135ac56d7e
sourize <sourishchatterjee0067@gmail.com>
2024-11-18 19:35:54 +0530

    Files Updated

---
+++ src/pages/_app.tsx
@@ -0,0 +1,25 @@
+import { AppProps } from 'next/app';
+import Head from 'next/head';
+import React from 'react';
+
+function MyApp({ Component, pageProps }: AppProps) {
+  return (
+    <>
+      <Head>
+        {/* Google tag (gtag.js) */}
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-J41N4GVFYJ"></script>
+        <script>
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-J41N4GVFYJ');
+          `}
+        </script>
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
+}
+
+export default MyApp;
