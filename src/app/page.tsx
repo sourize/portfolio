919c3ba224723ca0bd8b188e774cab51a05e2bad
sourize <sourishchatterjee0067@gmail.com>
2024-12-06 16:09:34 +0530

    Files Updated

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -32,7 +32,7 @@ interface BlogsI {
 
 export default function Page() {
   return (
-    <main className="flex flex-col min-h-[50dvh] space-y-2">
+    <main className="flex flex-col min-h-[100dvh] space-y-6">
       <section id="hero" className="mt-5">
         <div className="mx-auto w-full max-w-4xl space-y-8">
           <div className="gap-2 flex justify-between">
