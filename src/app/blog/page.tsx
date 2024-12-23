aaf31da243f3ed9e5567f42044fa1c4be922e702
sourize <sourishchatterjee0067@gmail.com>
2024-12-24 01:35:56 +0530

    Files Updated

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -4,8 +4,8 @@ import { getBlogPosts } from "@/data/blog";
 import Link from "next/link";
 
 export const metadata = {
-  title: "My Blogs",
-  description: "sharing thoughts and insights while exploring tech, life, and everything in between....",
+  title: "Blog",
+  description: "My thoughts on machine learning, life, and more.",
 };
 
 const BLUR_FADE_DELAY = 0.04;
@@ -21,10 +21,10 @@ export default async function BlogPage() {
             <div className="flex flex-col items-center justify-center space-y-4 text-center">
               <div className="space-y-2">
                 <h2 className="text-3xl font-bold tracking-tighter sm:text-5xl">
-                  My Blogs
+                  Thoughts & Insights
                 </h2>
                 <p className="text-muted-foreground md:text-xl/relaxed lg:text-base/relaxed xl:text-xl/relaxed">
-                  sharing thoughts and insights while exploring tech, life, and everything in between....
+                  Exploring tech, life, and everything in between. Join me as I share my perspectives on various topics. I hope you find something intriguing!
                 </p>
               </div>
             </div>
